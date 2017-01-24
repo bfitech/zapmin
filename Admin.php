7c6c44d131133959985ba7491b8c6cfc4dce42fe
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-01-24 11:08:48 +0700

    Add unit tests for AdminStore.

--- Admin.php
+++ Admin.php
@@ -52,6 +52,16 @@ class AdminStore {
 		$expire = $sql->stmt_fragment(
 			'datetime', ['delta' => $this->expiration]);
 
+		foreach([
+			"DROP VIEW IF EXISTS v_usess;",
+			"DROP TABLE IF EXISTS usess;",
+			"DROP TABLE IF EXISTS udata;",
+		] as $drop) {
+			if (!$sql->query_raw($drop))
+				throw new StorageError(
+					"Cannot drop data:" . $sql->errmsg);
+		}
+
 		$user_table = (
 			"CREATE TABLE udata (" .
 			"  uid %s," .
@@ -80,7 +90,7 @@ class AdminStore {
 		$session_table = (
 			"CREATE TABLE usess (" .
 			"  sid %s," .
-			"  uid INTEGER REFERENCES user(uid) ON DELETE CASCADE," .
+			"  uid INTEGER REFERENCES udata(uid) ON DELETE CASCADE," .
 			"  token VARCHAR(64)," .
 			"  expire DATE NOT NULL DEFAULT %s" .
 			") %s;"
@@ -125,19 +135,22 @@ class AdminStore {
 	 * is available.
 	 */
 	public function status() {
+		if ($this->user_token === null)
+			return null;
 		if ($this->user_data !== null)
 			return $this->user_data;
-		if ($this->user_token === null)
-			return $this->user_data = [];
 		$session = $this->sql->query(
 			sprintf(
 				"SELECT * FROM v_usess " .
 				"WHERE token=? AND expire>%s " .
 				"LIMIT 1",
-				$this->sql->stmt_frament('datetime')
+				$this->sql->stmt_fragment('datetime')
 			), [$this->user_token]);
-		if (!$session)
-			return $this->user_data = [];
+		if (!$session) {
+			# session not found or expired
+			$this->status_reset();
+			return $this->user_data;
+		}
 		return $this->user_data = $session;
 	}
 
@@ -154,12 +167,11 @@ class AdminStore {
 	}
 
 	/**
-	 * Shorthand for nullifying $this->user_data.
-	 *
-	 * Use this post-signout or the like to clear cache.
+	 * Shorthand for nullifying session data.
 	 */
-	public function status_reset() {
+	private function status_reset() {
 		$this->user_data = null;
+		$this->user_token = null;
 	}
 
 	private function match_password($uname, $upass, $usalt) {
@@ -178,6 +190,23 @@ class AdminStore {
 		return $this->generate_secret($upass . $uname, $usalt);
 	}
 
+	/**
+	 * Verify plaintext password.
+	 *
+	 * Used by password change, registration, password reset, etc.
+	 */
+	private function verify_password($pass1, $pass2) {
+		$pass1 = trim($pass1);
+		$pass2 = trim($pass2);
+		# type twice the same
+		if ($pass1 != $pass2)
+			return 1;
+		# must be longer than 3
+		if (strlen($pass1) < 4)
+			return 2;
+		return 0;
+	}
+
 	private function generate_secret($data, $key=null) {
 		if (!$key)
 			$key = (string)time();
@@ -189,6 +218,8 @@ class AdminStore {
 
 	/**
 	 * Sign in.
+	 *
+	 * @param array $arg Post data with keys: 'uname', 'upass'.
 	 */
 	public function login($args) {
 		if ($this->is_logged_in())
@@ -197,20 +228,21 @@ class AdminStore {
 		if (!isset($args['post']))
 			return [2];
 		if (!self::check_keys($args['post'], ['uname', 'upass']))
-			return [2];
+			return [3];
 		extract($args['post'], EXTR_SKIP);
 
 		$usalt = $this->sql->query(
 			"SELECT usalt FROM udata WHERE uname=? LIMIT 1",
 			[$uname]);
 		if (!$usalt)
-			return [3];
+			# user not found
+			return [4];
 		$usalt = $usalt['usalt'];
 
 		$udata = $this->match_password($uname, $upass, $usalt);
 		if (!$udata)
-			return [4];
-		$this->user_data = $udata;
+			# wrong password
+			return [5];
 
 		// generate token
 		$token = $this->generate_secret($upass . $usalt . time(), $usalt);
@@ -239,12 +271,12 @@ class AdminStore {
 	}
 
 	private function close_session($sid) {
-		$now = $thi->sql->query(
+		$now = $this->sql->query(
 			sprintf(
 				"SELECT %s AS now",
-				$this->sql->stmt_frament('datetime')
+				$this->sql->stmt_fragment('datetime')
 			), [], false);
-		return $this->sql->update('usess', [
+		$this->sql->update('usess', [
 			'expire' => $now['now'],
 		], [
 			'sid' => $sid
@@ -257,12 +289,14 @@ class AdminStore {
 	public function logout() {
 		if (!$this->is_logged_in())
 			return [1];
-		if (!$this->close_session($this->user_data['sid']))
-			# session not found
-			return [2];
+		# this just close sessions with current sid, whether
+		# it exists or not, possibly deleted by account
+		# self-delete
+		$this->close_session($this->user_data['sid']);
+		# reset status
 		$this->status_reset();
-		# router must set appropriate local storage, cookie, etc.
-		# e.g.: setcookie('cookie_adm', '', time() - 7200, '/');
+		# router must set appropriate cookie, e.g.:
+		# setcookie('cookie_adm', '', time() - 7200, '/');
 		return [0];
 	}
 
@@ -283,27 +317,24 @@ class AdminStore {
 
 		if (!isset($args['post']))
 			return [2];
-		if (!self::check_keys($keys, $args['post']))
+		$post = self::check_keys($args['post'], $keys);
+		if (!$post)
 			return [2];
-		extract($args['post'], EXTR_SKIP);
+		extract($post, EXTR_SKIP);
 
 		extract($this->user_data, EXTR_SKIP);
 
 		# check old password if applicable
 		if (
 			$with_old_password &&
-			!$this->match_password($uname, $upass, $usalt)
+			!$this->match_password($uname, $pass0, $usalt)
 		) {
 			return [3];
 		}
 
-		# type twice the same
-		if ($pass1 != $pass2)
-			return [4];
-
-		# must be longer than 3
-		if (strlen($pass1) < 4)
-			return [5];
+		$verify_password = $this->verify_password($pass1, $pass2);
+		if ($verify_password !== 0)
+			return [4, $verify_password];
 
 		# update
 		$this->sql->update('udata', [
@@ -343,56 +374,87 @@ class AdminStore {
 		$this->sql->update('udata', $vars, [
 			'uid' => $this->user_data['uid']
 		]);
+
+		# reset user data but not user token
+		$this->user_data = null;
+		$this->status();
+
+		# ok
 		return [0];
 	}
 
 	/**
 	 * Register a new user.
 	 *
-	 * @param bool $must_authz Only authorized user can register new
-	 *     one. Falsify to allow self-registration.
-	 * @param function $callback_authz A function that must return true
-	 *     to allow registration to proceed. Not used if $must_authz is
-	 *     false. Default to current user being root.
+	 * @param array $args Post data with keys: 'addname', 'addpass1',
+	 *     and optional 'addpass2' unless $pass_twice is set to true.
+	 * @param bool $pass_twice Whether password must be entered twice.
+	 * @param bool $allow_self_register Whether self-registration is
+	 *     allowed.
+	 * @param function $callback_authz A function that takes one parameter
+	 *     $callback_param to allow registration to proceed. Default to
+	 *     current user being root.
+	 * @param array $callback_param Parameter to pass to $callback_authz.
 	 */
-	public function add_user($args, $must_authz=true, $callback_authz=null) {
-
-		# authn
-		if ($must_authz) {
-			# must authn
-			if (!$this->is_logged_in())
-				return [1];
-			# authz check
-			if ($callback_authz === null) {
-				$callback_authz = function() {
-					return $this->user_data['uid'] == 1;
+	public function add_user(
+		$args, $pass_twice=false, $allow_self_register=false,
+		$callback_authz=null, $callback_param=null
+	) {
+		if ($this->is_logged_in()) {
+			if (!$callback_authz) {
+				# default callback
+				$callback_authz = function($param) {
+					# allow uid=1 only
+					if ($param['uid'] == 1)
+						return 0;
+					return 1;
 				};
+				$callback_param = [
+					'uid' => $this->user_data['uid']];
 			}
-			# must authz
-			if (!$callback_authz())
+			$ret = $callback_authz($callback_param);
+			if ($ret !== 0)
+				return [1, $ret];
+		} else {
+			# not signed in
+			if (!$allow_self_register)
+				# self-registration not allowed
 				return [2];
+			# NOTE: Other constraint such as captcha happens outside
+			# of this class.
 		}
 
 		# check vars
 		if (!isset($args['post']))
 			return [3];
-		$keys = ['addname', 'addpass'];
-		if (!self::check_keys($args['post'], $keys))
+		$keys = ['addname', 'addpass1'];
+		if ($pass_twice)
+			$keys[] = 'addpass2';
+		$post = self::check_keys($args['post'], $keys);
+		if (!$post)
 			return [3];
 		extract($args['post'], EXTR_SKIP);
 
+		if (!$pass_twice)
+			$addpass2 = $addpass1;
+		$verify_password = $this->verify_password($addpass1, $addpass2);
+		if ($verify_password !== 0)
+			return [4, $verify_password];
+
 		# hashes generation
-		$usalt = $this->generate_secret($addname . $addpass);
-		$hpass = $this->hash_password($addname, $addpass, $usalt);
+		$usalt = $this->generate_secret($addname . $addpass1);
+		$hpass = $this->hash_password($addname, $addpass1, $usalt);
 
 		# insert
 		if (!$this->sql->insert('udata', [
 			'uname' => $addname,
 			'upass' => $hpass,
 			'usalt' => $usalt,
-		]))
+		], 'uid')) {
 			# user exists
-			return [4];
+			# FIXME: Relying on PDO exception seems fishy.
+			return [6];
+		}
 
 		# success
 		return [0];
@@ -401,12 +463,19 @@ class AdminStore {
 	/*
 	 * Delete a user.
 	 *
+	 * @param array $args Post data with keys: 'uid'.
 	 * @param int $uid User ID to be deleted.
 	 * @param function $callback_authz Callback to determine whether
 	 *     current user is allowed to delete. Defaults to root and
 	 *     self-delete.
+	 * @param function $callback_authz A function that takes one parameter
+	 *     $callback_args to allow deletion to proceed. Default to current
+	 *     user being root, or non-root self-deletion.
+	 * @param array $callback_param Parameter to pass to $callback_authz.
 	 */
-	public function delete_user($args, $callback_authz=null) {
+	public function delete_user(
+		$args, $callback_authz=null, $callback_param=null
+	) {
 		if (!$this->is_logged_in())
 			return [1];
 		$user_data = $this->user_data;
@@ -418,20 +487,32 @@ class AdminStore {
 		extract($args['post'], EXTR_SKIP);
 
 		if (!$callback_authz) {
-			$callback_authz = function() {
-				if ($user_data['uid'] == 1 && $uid != 1)
-					return true;
-				if ($user_data['uid'] == $uid)
-					return true;
-				return false;
+			# default callback
+			$callback_authz = function($param) {
+				if ($param['own_uid'] == 1 && $param['del_uid'] != 1)
+					return 0;
+				if ($param['own_uid'] == $param['del_uid'])
+					return 0;
+				return 1;
 			};
+			$callback_param = [
+				'own_uid' => $user_data['uid'],
+				'del_uid' => $uid,
+			];
 		}
-		if (!$callback_authz())
-			return [3];
+		$ret = $callback_authz($callback_param);
+		if ($ret !== 0)
+			return [3, $ret];
 
 		# cannot delete root
 		if ($uid == 1)
 			return [4];
+
+		if (!$this->sql->query(
+			"SELECT uid FROM udata WHERE uid=? LIMIT 1",
+			[$uid]))
+			# user doesn't exist
+			return [5];
 
 		# kill all sessions; this should be faster
 		# with raw SQL, oh well
@@ -439,18 +520,16 @@ class AdminStore {
 			sprintf(
 				"SELECT sid FROM usess WHERE ".
 					"uid=? AND expire>=%s",
-				$this->sql->stmt_frament('datetime')
-			), [$uid]);
+				$this->sql->stmt_fragment('datetime')
+			), [$uid], true);
 		foreach ($sessions as $s)
 			$this->close_session($s['sid']);
 
-		# delete
-		if (!$this->sql->delete('udata', ['uid' => $uid]))
-			# user doesn't exist
-			return [5];
+		# delete user data
+		$this->sql->delete('udata', ['uid' => $uid]);
 
-		# in case of self-delete, router must do $this->status_reset()
-		# and send location.reload or the like
+		# in case of self-delete, router must send redirect header
+		# or location.reload from the client side
 		return [0];
 	}
 
@@ -459,16 +538,37 @@ class AdminStore {
 	 *
 	 * @param int $page Page number.
 	 * @param int $limit Limit per page.
+	 * @param function $callback_authz A function that takes one parameter
+	 *     $callback_args to allow listing. Default to current user being root.
+	 * @param array $callback_param Parameter to pass to $callback_authz.
 	 */
-	public function list_user($page=0, $limit=10) {
+	public function list_user(
+		$page=0, $limit=10, $callback_authz=null, $callback_param=null
+	) {
+		if (!$callback_authz) {
+			$this->is_logged_in();
+			$callback_authz = function($param) {
+				# allow uid=1 only
+				if ($param['uid'] == 1)
+					return 0;
+				return 1;
+			};
+			$callback_param = [
+				'uid' => $this->user_data['uid']];
+		}
+		$ret = $callback_authz($callback_param);
+		if ($ret !== 0)
+			return [1, $ret];
 
 		if ($page < 0)
 			$page = 0;
 		$offset = $page * $limit;
-		return $this->sql->query(
-			"SELECT uid, uname, fname, site, since " .
-			"FROM udata ORDER BY uid LIMIT ? OFFSET ?",
-			[$limit, $offset]);
+		return [0,
+			$this->sql->query(
+				"SELECT uid, uname, fname, site, since " .
+				"FROM udata ORDER BY uid LIMIT ? OFFSET ?",
+				[$limit, $offset], true)
+		];
 	}
 }
 
---
+++ tests/AdminStoreTest.php
@@ -0,0 +1,384 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use BFITech\ZapStore as zs;
+use BFITech\ZapAdmin as za;
+
+
+class AdminTest extends TestCase {
+
+	protected static $sql;
+	protected static $store;
+
+	public static function postFormatter($args) {
+		return ['post' => $args];
+	}
+
+	public static function loginOK($uname='root', $upass='admin') {
+		$result = self::$store->login(self::postFormatter([
+			'uname' => $uname,
+			'upass' => $upass,
+		]));
+		self::$store->set_user_token($result[1]['token']);
+		self::$store->status();
+	}
+
+	public static function setUpBeforeClass() {
+		self::$sql = new zs\SQL([
+			'dbtype' => 'sqlite3',
+			'dbname' => ':memory:'
+		]);
+		self::$sql->open();
+		self::$store= new za\AdminStore(self::$sql, 600, true);
+	}
+
+	public static function tearDownAfterClass() {
+		#self::$sql = null;
+	}
+
+	public function tearDown() {
+		if (self::$store->get_safe_user_data())
+			self::$store->logout();
+	}
+
+	public function test_connection() {
+		$this->assertEquals(
+			self::$sql->get_connection_params()['dbtype'], 'sqlite3');
+	}
+
+	public function test_check_keys() {
+		$array = [
+			'foo' => 1,
+			'bar' => 2,
+		];
+		$keys = ['foo', 'bar'];
+		$this->assertNotEquals(
+			self::$store->check_keys($array, $keys), false);
+		$keys[] = 'baz';
+		$this->assertEquals(
+			self::$store->check_keys($array, $keys), false);
+	}
+
+	public function test_create_table() {
+		$qr = self::$sql->query(
+			"SELECT uname FROM udata LIMIT 1");
+		$this->assertEquals($qr['uname'], 'root');
+	}
+
+	public function test_set_user_token() {
+		# user data is empty array
+		$this->assertEquals(
+			self::$store->get_safe_user_data(), []);
+
+		# set an invalid token
+		self::$store->set_user_token('invalid token');
+
+		# reset status
+		self::$store->status();
+
+		# user data is still empty array
+		$this->assertEquals(
+			self::$store->get_safe_user_data(), []);
+
+		# cannot sing out with no valid session
+		$this->assertEquals(
+			self::$store->logout()[0], 1);
+	}
+
+	public function test_login() {
+		# invalid post data
+		$args = ['uname' => 'admin'];
+		$this->assertEquals(
+			self::$store->login($args)[0], 2);
+
+		# incomplete post data
+		$args = self::postFormatter($args);
+		$this->assertEquals(
+			self::$store->login($args)[0], 3);
+
+		# invalid user
+		$args['post']['upass'] = '1243';
+		$this->assertEquals(
+			self::$store->login($args)[0], 4);
+
+		# invalid password
+		$args['post']['uname'] = 'root';
+		$this->assertEquals(
+			self::$store->login($args)[0], 5);
+
+		# success
+		$args['post']['upass'] = 'admin';
+		$login_data = self::$store->login($args);
+		$this->assertEquals($login_data[0], 0);
+		$token = $login_data[1]['token'];
+
+		# simulating next load
+		self::$store->set_user_token($token);
+		self::$store->status();
+		# re-login will fail
+		$this->assertEquals(
+			self::$store->login($args)[0], 1);
+	}
+
+	public function test_logout() {
+		$this->assertEquals(self::$store->logout()[0], 1);
+		self::loginOK();
+		$this->assertEquals(self::$store->logout()[0], 0);
+	}
+
+	public function test_change_password() {
+		# not logged in
+		$args = ['pass1' => '123'];
+		$this->assertEquals(
+			self::$store->change_password($args)[0], 1);
+
+		self::loginOK();
+
+		# invalid data
+		$this->assertEquals(
+			self::$store->change_password($args)[0], 2);
+
+		# incomplete data
+		$args['pass2'] = '1234';
+		$args = self::postFormatter($args);
+		$this->assertEquals(
+			self::$store->change_password($args, true)[0], 2);
+
+		# wrong old password
+		$args['post']['pass0'] = '1234';
+		$this->assertEquals(
+			self::$store->change_password($args, true)[0], 3);
+
+		# new passwords don't verify
+		$args['post']['pass0'] = 'admin';
+		$this->assertEquals(
+			self::$store->change_password($args, true)[0], 4);
+		$this->assertEquals(
+			self::$store->change_password($args, true)[1], 1);
+
+		# new password too short
+		$args['post']['pass2'] = '123';
+		$this->assertEquals(
+			self::$store->change_password($args, true)[0], 4);
+		$this->assertEquals(
+			self::$store->change_password($args, true)[1], 2);
+
+		# success
+		$args['post']['pass1'] = '1234';
+		$args['post']['pass2'] = '1234';
+		$this->assertEquals(
+			self::$store->change_password($args, true)[0], 0);
+
+		# logout
+		$this->assertEquals(self::$store->logout()[0], 0);
+
+		# relogin with old password will fail
+		try {
+			self::loginOK();
+		} catch (Exception $e) {
+			$this->assertEquals(self::$store->status(), null);
+		}
+
+		# login with new password
+		self::loginOK('root', '1234');
+
+		# change password back without old password requirement
+		$args['post']['pass1'] = $args['post']['pass2'] = 'admin';
+		$this->assertEquals(
+			self::$store->change_password($args)[0], 0);
+	}
+
+	public function test_change_bio() {
+		self::loginOK();
+		$safe_data = self::$store->get_safe_user_data();
+		$this->assertEquals($safe_data['fname'], '');
+
+		# change fname
+		$r = self::$store->change_bio([
+			'post' => [
+				'fname' => 'The Administrator']]);
+
+		$safe_data = self::$store->get_safe_user_data();
+		$this->assertEquals($safe_data['site'], '');
+		$this->assertEquals($safe_data['fname'], 'The Administrator');
+	}
+
+	public function test_self_register() {
+		$args = ['post' => [
+			'addname' => 'root',
+			'addpass1' => 'asdf',
+			'addpass2' => 'asdf']];
+
+		# user exists
+		$this->assertEquals(
+			self::$store->add_user($args, true, true)[0], 6);
+
+		# success
+		$args['post']['addname'] = 'john';
+		$this->assertEquals(
+			self::$store->add_user($args, true, true)[0], 0);
+		# autologin, this should happen immediately prior to
+		# sending anything to client
+		self::loginOK('john', 'asdf');
+		$user_data = self::$store->status();
+		$this->assertEquals($user_data['uname'], 'john');
+	}
+
+	/**
+	 * @depends test_self_register
+	 */
+	public function test_register() {
+		$args = ['post' => [
+			'addname' => 'john',
+			'addpass1' => 'asdf']];
+
+		# no authn, self-registration disabled
+		$this->assertEquals(
+			self::$store->add_user($args, false, false)[0], 2);
+
+		# as 'john' with default callback
+		self::loginOK('john', 'asdf');
+		# no authz
+		$this->assertEquals(
+			self::$store->add_user($args)[0], 1);
+		$this->assertEquals(
+			self::$store->add_user($args)[1], 1);
+		self::$store->logout();
+
+		# as root, with unavailable name
+		self::loginOK();
+		# user exists
+		$this->assertEquals(
+			self::$store->add_user($args)[0], 6);
+
+		# as root, with available name
+		$args['post']['addname'] = 'jocelyn';
+		# success, no autologin
+		$this->assertEquals(
+			self::$store->add_user($args)[0], 0);
+		self::$store->logout();
+
+		# with callback
+		$cbf = function($_args) {
+			# only 'root' and 'john' are allowed to add new user
+			if (in_array($_args['uname'], ['root', 'john']))
+				return 0;
+			return 1;
+		};
+
+		# try to add 'jonah'
+		$args['post']['addname'] = 'jonah';
+		$args['post']['addpass1'] = '123';
+
+		# as 'jocelyn'
+		self::loginOK('jocelyn', 'asdf');
+		$uname = self::$store->get_safe_user_data()['uname'];
+		$cbp = ['uname' => $uname];
+		# no authz, doesn't satisfy callback
+		$this->assertEquals(
+			self::$store->add_user(
+				$args, false, false, $cbf, $cbp)[0], 1);
+		$this->assertEquals(
+			self::$store->add_user(
+				$args, false, false, $cbf, $cbp)[1], 1);
+		self::$store->logout();
+
+		# as 'john'
+		self::loginOK('john', 'asdf');
+		$uname = self::$store->get_safe_user_data()['uname'];
+		$cbp = ['uname' => $uname];
+		# pass authz but password doesn't check out
+		$this->assertEquals(
+			self::$store->add_user(
+				$args, false, false, $cbf, $cbp)[0], 4);
+		$this->assertEquals(
+			self::$store->add_user(
+				$args, false, false, $cbf, $cbp)[1], 2);
+
+		# as 'john'
+		$args['post']['addpass1'] = 'asdfgh';
+		# success
+		$this->assertEquals(
+			self::$store->add_user(
+				$args, false, false, $cbf, $cbp)[0], 0);
+		self::$store->logout();
+
+		# try sign in as 'jonah'
+		self::loginOK('jonah', 'asdfgh');
+	}
+
+	/**
+	 * @depends test_register
+	 */
+	public function test_delete_user() {
+		$args = self::postFormatter(['uid' => 0]);
+
+		self::loginOK();
+		$user_list = self::$store->list_user()[1];
+		# so far we have 4 users
+		$this->assertEquals(count($user_list), 4);
+		self::$store->logout();
+
+		# create uid arrays
+		$uids = array_map(function($_arr){
+			return $_arr['uid'];
+		}, $user_list);
+
+		# no authn
+		$this->assertEquals(
+			self::$store->delete_user($args)[0], 1);
+
+		# as 'jonah'
+		self::loginOK('jonah', 'asdfgh');
+		# with default callback, any user cannot delete another user 
+		# except root
+		$this->assertEquals(
+			self::$store->delete_user($args)[0], 3);
+		# but s/he can self-delete
+		$args['post']['uid'] = self::$store->status()['uid'];
+		$this->assertEquals(
+			self::$store->delete_user($args)[0], 0);
+		# logout is still allowed since it doesn't check sid
+		self::$store->logout();
+		# unable to re-login because user is no longer found
+		$this->assertEquals(self::$store->login(self::postFormatter([
+			'uname' => 'jonah',
+			'upass' => 'asdfgh',
+		]))[0], 4);
+
+		# with callback
+		$cbf = function($_args) {
+			# only 'root' and 'john' are allowed to delete
+			if (in_array($_args['uname'], ['root', 'john']))
+				return 0;
+			return 1;
+		};
+
+		# as john
+		self::loginOK('john', 'asdf');
+		$uname = self::$store->get_safe_user_data()['uname'];
+		$cbp = ['uname' => $uname];
+		# user doesn't exist
+		$this->assertEquals(
+			self::$store->delete_user($args, $cbf, $cbp)[0], 5);
+		# cannot delete 'root'
+		$args['post']['uid'] = 1;
+		$this->assertEquals(
+			self::$store->delete_user($args, $cbf, $cbp)[0], 4);
+		# success, delete 'jocelyn' uid=3
+		$args['post']['uid'] = 3;
+		$this->assertEquals(
+			self::$store->delete_user($args, $cbf, $cbp)[0], 0);
+		self::$store->logout();
+
+		# sign in as 'jocelyn' fails
+		try {
+			self::loginOK('jocelyn', '1234');
+		} catch (Exception $e) {
+			$this->assertEquals(self::$store->status(), null);
+		}
+	}
+}
+
+

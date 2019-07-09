549e0e34a5345d17aa9d39ffe7864713caed7961
Rendra Suroso <rs@cogsci.bandungfe.net>
2019-07-10 02:59:01 +0700

    Make AuthManage param optional on Route.

--- src/Route.php
+++ src/Route.php
@@ -11,11 +11,10 @@ use BFITech\ZapCore\Router;
  * Route class.
  *
  * This is a thin layer than glues router and storage together.
- * Subclassess extend this.
  *
- * @see RouteDefault for limited example.
+ * @see RouteDefault for sample implemetation.
  */
-class Route {
+abstract class Route {
 
 	/** Router instancs */
 	public static $core;
@@ -34,10 +33,11 @@ class Route {
 	 *
 	 * @param Router $core Router instance.
 	 * @param AuthCtrl $ctrl AuthCtrl instance.
-	 * @param AuthManage $manage AuthManage instance.
+	 * @param AuthManage $manage AuthManage instance. Leave this black
+	 *     if you don't care about user management.
 	 */
 	public function __construct(
-		Router $core, AuthCtrl $ctrl, AuthManage $manage
+		Router $core, AuthCtrl $ctrl, AuthManage $manage=null
 	) {
 		self::$core = $core;
 		self::$ctrl = $ctrl;
@@ -47,32 +47,38 @@ class Route {
 		$this->expiration = $ctrl::$admin->get_expiration();
 	}
 
+	private function _set_token_value($token_value) {
+		self::$ctrl->set_token_value($token_value);
+		if (self::$manage)
+			self::$manage->set_token_value($token_value);
+	}
+
 	/**
 	 * Standard wrapper for Router::route.
 	 *
 	 * @param string $path Router path.
 	 * @param callable $callback Router callback.
 	 * @param string|array $method Router request method.
-	 * @param bool $is_raw If true, accept raw request body, POST only.
+	 * @param bool $is_raw If true, accept raw request body. For POST
+	 *     request only.
 	 */
 	public function route(
 		string $path, callable $callback, $method='GET',
 		bool $is_raw=null
 	) {
-		static::$core->route($path, function($args) use($callback) {
+		self::$core->route($path, function($args) use($callback) {
 			$token_name = $this->token_name;
+			$cookie = $args['cookie'];
 			# set token if available
-			if (isset($args['cookie'][$token_name])) {
+			if (isset($cookie[$token_name])) {
 				# via cookie
-				static::$ctrl->set_token_value(
-					$args['cookie'][$token_name]);
-				static::$manage->set_token_value(
-					$args['cookie'][$token_name]);
+				$this->_set_token_value($cookie[$token_name]);
 			} elseif (isset($args['header']['authorization'])) {
 				# via request header
 				$auth = explode(' ', $args['header']['authorization']);
-				if (count($auth) == 2 && $auth[0] == $token_name)
-					static::$ctrl->set_token_value($auth[1]);
+				if (count($auth) == 2 && $auth[0] == $token_name) {
+					$this->_set_token_value($auth[1]);
+				}
 			}
 			# execute calback
 			$callback($args);
